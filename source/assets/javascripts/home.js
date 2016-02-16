75123fe1c97a6860d7f070d96e6a234db798a694
Paulo Favato <favatorama@gmail.com>
2016-02-16 16:46:51 -0200

    Updates

--- source/assets/javascripts/home.js
+++ source/assets/javascripts/home.js
@@ -1,9 +1,9 @@
 // slider - últimos projetos desenvolvidos
 $(document).ready(function() {
   $("#js-our-projects").owlCarousel({
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem : true
+    navigation : true, // Show next and prev buttons
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem : true
   });
 });
